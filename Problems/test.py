6fa6bfc2059fa6b263101fa3f9212c1ce961d8b4
Mithzzx <Mithzzx@users.noreply.github.com>
2024-04-16 23:00:46 +0530

    Update test.py

--- Problems/test.py
+++ Problems/test.py
@@ -1,9 +1,17 @@
-a=[1,2,3,4,5]
+import requasts
 
-
-def reverseArray(a):
-    a.reverse()
-    return a
-
-
-reverseArray(a)
+# Define the API endpoint
+api_endpoint = "https://jsonplaceholder.typicode.com/posts"
+# Nake a GET request to the API endpoint
+response = requests - get(apiendpoint)
+# Check if the request wus successful （status code 290）
+if resporse.status_code == 200:
+    # Parse the 】saN response
+    data = response.ison()
+    # Print the ferched data
+    for post in data:
+        print("Title:", post["title"])
+        print("Body:".post["body"])
+        print("---------------------")
+else:
+    print("Failed to fetch data. Status code:", response, status_code)
