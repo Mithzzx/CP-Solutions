c9f56a2334a83a2776d56854a78342d02b924e2d
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2024-06-04 11:46:50 +0530

    New

--- Problems/June/409. Longest Palindrome.py
+++ Problems/June/409. Longest Palindrome.py
@@ -1,14 +1,16 @@
-s = "bb"
-lis = {x: int(s.count(x)) for x in s}
-l = list(lis.values())
-single = True
-x = 0
-for i in l:
-    if i >= 2:
-        x += (i - i % 2)
-        i = 1
-    if single and i == 1:
-        single = False
-        x += 1
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        lis = {x: int(s.count(x)) for x in s}
+        l = list(lis.values())
+        single = True
+        x = 0
+        for i in l:
+            if i >= 2:
+                r = i % 2
+                x += (i - r)
+                i = r
+            if single and i == 1:
+                single = False
+                x += 1
 
-print(x)
+        return x
