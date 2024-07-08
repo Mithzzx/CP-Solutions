6a7fdb92e0978d265ba38c2be4c73c8e6bd26e3c
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2024-07-08 22:24:58 +0530

    1823

--- Problems/Calender.py
+++ Problems/Calender.py
@@ -1,3 +1,24 @@
-from playsound import playsound
-falcfile = input("Enter filename :")
-playsound(falcfile)
+import pyautogui as auto
+from time import sleep
+import sys
+
+x = []
+
+sleep(3)
+
+l = len(x)
+p = 0
+c = 0
+for i in x:
+    auto.write(i[:-2])
+    auto.press('enter')
+
+    s = str(c) + ":| Line" + str(c + 4) + '\n'
+    c += 1
+
+    p = c / l * 100
+    b = ("Loading.. " + str("%.4f" % p) + '%')
+    # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    sys.stdout.write('\r' + s+b)
+
+    sleep(8)
