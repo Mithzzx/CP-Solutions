dd3781acee20adc18fc32b2aa1341c2d1e57417f
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2025-03-14 09:45:30 +0530

    new

---
+++ CodeChef/Practice/Even-tual Reduction.py
@@ -0,0 +1,23 @@
+# Problem: CodeChef Even-tual Reduction
+# Link: https://www.codechef.com/problems/EVENTUAL
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# Approach: Count the frequency of each character and check if the frequency of each character is even or not
+# Data Structure: Dictionary
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(input())
+    d = {}
+    for i in a:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    for j in d:
+        if d[j] % 2 != 0:
+            print("NO")
+            break
+        else:
+            print("YES")
